f710da82444afcbe17f0c201aa406a04d7219444
samer <samer_mail_2006@yahoo.com>
2017-05-25 23:18:26 +0300

    Add acount Class.

---
+++ src/AccountSystem.Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AccountSystem.Entities.Enum;
+
+namespace AccountSystem.Entities
+{
+    public class Account : EntityBase
+    {
+        public int AccountId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int? ParentId { get; set; }
+        public decimal InitalBalance { get; set; }
+        public AccountType Type { get; set; }
+        public string Note { get; set; }
+
+    }
+}
---
+++ src/AccountSystem.Entities/Enum/AccountType.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountSystem.Entities.Enum
+{
+    public enum AccountType
+    {
+        Credit,//دائن
+        Debit//مدين
+
+    }
+}
